a5704c04963b38c70c776f661ac588c36994d1ee
Isaque Dutra <dutra@mit.edu>
2014-08-03 00:45:50 -0400

    Added static image to home page.

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'home', view: 'index' });
+  res.render('about', { title: 'about', view: 'about' });
 });
 
 module.exports = router;
